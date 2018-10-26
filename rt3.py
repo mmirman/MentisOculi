eedd58a4cdac707120434483160203bac4779967
Matthew Mirman <matthew.mirman@inf.ethz.ch>
2018-10-26 14:18:40 +0200

    got the disks working

--- rt3.py
+++ rt3.py
@@ -21,7 +21,7 @@ def no_repeats(l):
 def save_img(args, img, nm):
     file_nm = os.path.join(args.SAVE_DIR,nm)
     print("\tsaving:", file_nm)
-    img = (img * 8.0 ).clamp(0,1) * 255
+    img = (img * 22.0 ).clamp(0,1) * 255
     img = img.transpose(0,2)
     
     rgb = [Image.fromarray(np.array(c), "F").resize((args.WIDTH, args.HEIGHT), Image.ANTIALIAS).convert("L") for c in img.float()]
@@ -440,17 +440,17 @@ class StaticArgs:
     SAVE_DIR="tmp"
     OVERSAMPLE = 2
 
-    SUBSAMPLE = 8
+    SUBSAMPLE = 10
 
     WIDTH = 300
     HEIGHT = 300
 
     scene = [
-        Light(vec3(0, 1.8, 0), 0.5, rgb(1, 1, 1)),
+        Light(vec3(0, 2.1, 0.5), 0.4, rgb(1, 1, 1)),
         #Light(vec3(-1.3, 1.7, 0.7), 0.5, rgb(1, 1, 1)),
         #Sphere(vec3(.3, .1, 1.3), .6, rgb(0.1, 0.1, 0), rgb(0.9, 0.95, 1)),
         Sphere(vec3(.42, .35, 0.8), 1.1, rgb(0.1, .1, .02), 0.98, semi=vec3(-1, -0.7,0.45), semi_rel = .45),
-        Sphere(vec3(.28, -0.2, 1.0), .3, rgb(0.1, 0.1, 0), rgb(0.7, 1, 1)),
+        Sphere(vec3(.3, -0.35, 0.9), .4, rgb(0.0, 0.0, 0), rgb(1, 1, 0.8), semi=vec3(1, 1.5,-0.45), semi_rel = .7),
         CheckeredSphere(vec3(0,-99999.5, 0), 99999, rgb(.99, .99, .99), diffuse2 = rgb(0.3, 0.3, 0.8)),
         Sphere(vec3(0, 100000.8, 0), 99999, rgb(0.99, 0.99, 0.99)),
         Sphere(vec3(0, 0, 100001.), 99999, rgb(0.99, 0.99, 0.99)),
@@ -460,14 +460,15 @@ class StaticArgs:
 
     eye = vec3(0., 0.35, -1.)     # Eye position
     FARAWAY = 1.0e36            # an implausibly huge distance
-    MAX_BOUNCE = 4
+    MAX_BOUNCE = 5
     NUDGE = 0.0000001
-    STOP_PROB = 0.9
+    STOP_PROB = 0.7
 
     NEAREST = 0.000000001
     restart_freq = 60
     mut_restart_freq = 20
     num_mc_samples = 20
-    jump_size = 0.005
+    jump_size = 0.006
 
-render(StaticArgs)
+with torch.no_grad():
+    render(StaticArgs)
